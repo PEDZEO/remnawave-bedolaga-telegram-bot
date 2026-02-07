3fd3bce2cfc4d9dee92ac43fbf55e4d9ebfe7fe4
Fringg <egor.andrianovv@gmail.com>
2026-02-07 11:29:31 +0300

    Revert "Merge pull request #2565 from BEDOLAGA-DEV/feat/traffic-filters-devices"
    This reverts commit ad6522f547e68ef5965e70d395ca381b0a032093, reversing
    changes made to 61bb8fcafd94509568f134ccdba7769b66cc7d5d.

--- app/cabinet/schemas/traffic.py
+++ app/cabinet/schemas/traffic.py
@@ -18,7 +18,6 @@ class UserTrafficItem(BaseModel):
     subscription_status: str | None
     traffic_limit_gb: float
     device_limit: int
-    connected_devices: int = 0
     node_traffic: dict[str, int]  # {node_uuid: total_bytes}
     total_bytes: int
 
@@ -31,17 +30,10 @@ class TrafficUsageResponse(BaseModel):
     limit: int
     period_days: int
     available_tariffs: list[str]
-    available_statuses: list[str]
 
 
 class ExportCsvRequest(BaseModel):
     period: int = Field(30, ge=1, le=30)
-    start_date: str = ''
-    end_date: str = ''
-    tariffs: str = ''
-    nodes: str = ''
-    statuses: str = ''
-    search: str = ''
 
 
 class ExportCsvResponse(BaseModel):
