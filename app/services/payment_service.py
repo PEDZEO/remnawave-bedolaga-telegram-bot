0d944e9222ca3a7311c608f54f17f162fa4b1e8e
PEDZEO <vitali.gavrikov@yandex.ru>
2026-02-22 05:01:37 +0300

    refactor: unify payment CRUD compatibility wrappers

--- app/services/payment_service.py
+++ app/services/payment_service.py
@@ -87,83 +87,67 @@ async def get_user_by_telegram_id(*args, **kwargs):
 
 
 async def create_mulenpay_payment(*args, **kwargs):
-    mulenpay_crud = import_module('app.database.crud.mulenpay')
-    return await mulenpay_crud.create_mulenpay_payment(*args, **kwargs)
+    return await _call_crud('app.database.crud.mulenpay', 'create_mulenpay_payment', *args, **kwargs)
 
 
 async def get_mulenpay_payment_by_uuid(*args, **kwargs):
-    mulenpay_crud = import_module('app.database.crud.mulenpay')
-    return await mulenpay_crud.get_mulenpay_payment_by_uuid(*args, **kwargs)
+    return await _call_crud('app.database.crud.mulenpay', 'get_mulenpay_payment_by_uuid', *args, **kwargs)
 
 
 async def get_mulenpay_payment_by_mulen_id(*args, **kwargs):
-    mulenpay_crud = import_module('app.database.crud.mulenpay')
-    return await mulenpay_crud.get_mulenpay_payment_by_mulen_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.mulenpay', 'get_mulenpay_payment_by_mulen_id', *args, **kwargs)
 
 
 async def get_mulenpay_payment_by_local_id(*args, **kwargs):
-    mulenpay_crud = import_module('app.database.crud.mulenpay')
-    return await mulenpay_crud.get_mulenpay_payment_by_local_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.mulenpay', 'get_mulenpay_payment_by_local_id', *args, **kwargs)
 
 
 async def update_mulenpay_payment_status(*args, **kwargs):
-    mulenpay_crud = import_module('app.database.crud.mulenpay')
-    return await mulenpay_crud.update_mulenpay_payment_status(*args, **kwargs)
+    return await _call_crud('app.database.crud.mulenpay', 'update_mulenpay_payment_status', *args, **kwargs)
 
 
 async def update_mulenpay_payment_metadata(*args, **kwargs):
-    mulenpay_crud = import_module('app.database.crud.mulenpay')
-    return await mulenpay_crud.update_mulenpay_payment_metadata(*args, **kwargs)
+    return await _call_crud('app.database.crud.mulenpay', 'update_mulenpay_payment_metadata', *args, **kwargs)
 
 
 async def link_mulenpay_payment_to_transaction(*args, **kwargs):
-    mulenpay_crud = import_module('app.database.crud.mulenpay')
-    return await mulenpay_crud.link_mulenpay_payment_to_transaction(*args, **kwargs)
+    return await _call_crud('app.database.crud.mulenpay', 'link_mulenpay_payment_to_transaction', *args, **kwargs)
 
 
 async def create_pal24_payment(*args, **kwargs):
-    pal_crud = import_module('app.database.crud.pal24')
-    return await pal_crud.create_pal24_payment(*args, **kwargs)
+    return await _call_crud('app.database.crud.pal24', 'create_pal24_payment', *args, **kwargs)
 
 
 async def get_pal24_payment_by_bill_id(*args, **kwargs):
-    pal_crud = import_module('app.database.crud.pal24')
-    return await pal_crud.get_pal24_payment_by_bill_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.pal24', 'get_pal24_payment_by_bill_id', *args, **kwargs)
 
 
 async def get_pal24_payment_by_order_id(*args, **kwargs):
-    pal_crud = import_module('app.database.crud.pal24')
-    return await pal_crud.get_pal24_payment_by_order_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.pal24', 'get_pal24_payment_by_order_id', *args, **kwargs)
 
 
 async def get_pal24_payment_by_id(*args, **kwargs):
-    pal_crud = import_module('app.database.crud.pal24')
-    return await pal_crud.get_pal24_payment_by_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.pal24', 'get_pal24_payment_by_id', *args, **kwargs)
 
 
 async def update_pal24_payment_status(*args, **kwargs):
-    pal_crud = import_module('app.database.crud.pal24')
-    return await pal_crud.update_pal24_payment_status(*args, **kwargs)
+    return await _call_crud('app.database.crud.pal24', 'update_pal24_payment_status', *args, **kwargs)
 
 
 async def link_pal24_payment_to_transaction(*args, **kwargs):
-    pal_crud = import_module('app.database.crud.pal24')
-    return await pal_crud.link_pal24_payment_to_transaction(*args, **kwargs)
+    return await _call_crud('app.database.crud.pal24', 'link_pal24_payment_to_transaction', *args, **kwargs)
 
 
 async def create_wata_payment(*args, **kwargs):
-    wata_crud = import_module('app.database.crud.wata')
-    return await wata_crud.create_wata_payment(*args, **kwargs)
+    return await _call_crud('app.database.crud.wata', 'create_wata_payment', *args, **kwargs)
 
 
 async def get_wata_payment_by_link_id(*args, **kwargs):
-    wata_crud = import_module('app.database.crud.wata')
-    return await wata_crud.get_wata_payment_by_link_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.wata', 'get_wata_payment_by_link_id', *args, **kwargs)
 
 
 async def get_wata_payment_by_id(*args, **kwargs):
-    wata_crud = import_module('app.database.crud.wata')
-    return await wata_crud.get_wata_payment_by_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.wata', 'get_wata_payment_by_id', *args, **kwargs)
 
 
 # Алиас для совместимости с хендлерами
@@ -172,118 +156,95 @@ async def get_wata_payment_by_local_id(*args, **kwargs):
 
 
 async def get_wata_payment_by_order_id(*args, **kwargs):
-    wata_crud = import_module('app.database.crud.wata')
-    return await wata_crud.get_wata_payment_by_order_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.wata', 'get_wata_payment_by_order_id', *args, **kwargs)
 
 
 async def update_wata_payment_status(*args, **kwargs):
-    wata_crud = import_module('app.database.crud.wata')
-    return await wata_crud.update_wata_payment_status(*args, **kwargs)
+    return await _call_crud('app.database.crud.wata', 'update_wata_payment_status', *args, **kwargs)
 
 
 async def link_wata_payment_to_transaction(*args, **kwargs):
-    wata_crud = import_module('app.database.crud.wata')
-    return await wata_crud.link_wata_payment_to_transaction(*args, **kwargs)
+    return await _call_crud('app.database.crud.wata', 'link_wata_payment_to_transaction', *args, **kwargs)
 
 
 async def create_platega_payment(*args, **kwargs):
-    platega_crud = import_module('app.database.crud.platega')
-    return await platega_crud.create_platega_payment(*args, **kwargs)
+    return await _call_crud('app.database.crud.platega', 'create_platega_payment', *args, **kwargs)
 
 
 async def get_platega_payment_by_id(*args, **kwargs):
-    platega_crud = import_module('app.database.crud.platega')
-    return await platega_crud.get_platega_payment_by_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.platega', 'get_platega_payment_by_id', *args, **kwargs)
 
 
 async def get_platega_payment_by_id_for_update(*args, **kwargs):
-    platega_crud = import_module('app.database.crud.platega')
-    return await platega_crud.get_platega_payment_by_id_for_update(*args, **kwargs)
+    return await _call_crud('app.database.crud.platega', 'get_platega_payment_by_id_for_update', *args, **kwargs)
 
 
 async def get_platega_payment_by_transaction_id(*args, **kwargs):
-    platega_crud = import_module('app.database.crud.platega')
-    return await platega_crud.get_platega_payment_by_transaction_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.platega', 'get_platega_payment_by_transaction_id', *args, **kwargs)
 
 
 async def get_platega_payment_by_correlation_id(*args, **kwargs):
-    platega_crud = import_module('app.database.crud.platega')
-    return await platega_crud.get_platega_payment_by_correlation_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.platega', 'get_platega_payment_by_correlation_id', *args, **kwargs)
 
 
 async def update_platega_payment(*args, **kwargs):
-    platega_crud = import_module('app.database.crud.platega')
-    return await platega_crud.update_platega_payment(*args, **kwargs)
+    return await _call_crud('app.database.crud.platega', 'update_platega_payment', *args, **kwargs)
 
 
 async def link_platega_payment_to_transaction(*args, **kwargs):
-    platega_crud = import_module('app.database.crud.platega')
-    return await platega_crud.link_platega_payment_to_transaction(*args, **kwargs)
+    return await _call_crud('app.database.crud.platega', 'link_platega_payment_to_transaction', *args, **kwargs)
 
 
 async def create_cryptobot_payment(*args, **kwargs):
-    crypto_crud = import_module('app.database.crud.cryptobot')
-    return await crypto_crud.create_cryptobot_payment(*args, **kwargs)
+    return await _call_crud('app.database.crud.cryptobot', 'create_cryptobot_payment', *args, **kwargs)
 
 
 async def get_cryptobot_payment_by_invoice_id(*args, **kwargs):
-    crypto_crud = import_module('app.database.crud.cryptobot')
-    return await crypto_crud.get_cryptobot_payment_by_invoice_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.cryptobot', 'get_cryptobot_payment_by_invoice_id', *args, **kwargs)
 
 
 async def update_cryptobot_payment_status(*args, **kwargs):
-    crypto_crud = import_module('app.database.crud.cryptobot')
-    return await crypto_crud.update_cryptobot_payment_status(*args, **kwargs)
+    return await _call_crud('app.database.crud.cryptobot', 'update_cryptobot_payment_status', *args, **kwargs)
 
 
 async def link_cryptobot_payment_to_transaction(*args, **kwargs):
-    crypto_crud = import_module('app.database.crud.cryptobot')
-    return await crypto_crud.link_cryptobot_payment_to_transaction(*args, **kwargs)
+    return await _call_crud('app.database.crud.cryptobot', 'link_cryptobot_payment_to_transaction', *args, **kwargs)
 
 
 async def create_heleket_payment(*args, **kwargs):
-    heleket_crud = import_module('app.database.crud.heleket')
-    return await heleket_crud.create_heleket_payment(*args, **kwargs)
+    return await _call_crud('app.database.crud.heleket', 'create_heleket_payment', *args, **kwargs)
 
 
 async def get_heleket_payment_by_uuid(*args, **kwargs):
-    heleket_crud = import_module('app.database.crud.heleket')
-    return await heleket_crud.get_heleket_payment_by_uuid(*args, **kwargs)
+    return await _call_crud('app.database.crud.heleket', 'get_heleket_payment_by_uuid', *args, **kwargs)
 
 
 async def get_heleket_payment_by_id(*args, **kwargs):
-    heleket_crud = import_module('app.database.crud.heleket')
-    return await heleket_crud.get_heleket_payment_by_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.heleket', 'get_heleket_payment_by_id', *args, **kwargs)
 
 
 async def update_heleket_payment(*args, **kwargs):
-    heleket_crud = import_module('app.database.crud.heleket')
-    return await heleket_crud.update_heleket_payment(*args, **kwargs)
+    return await _call_crud('app.database.crud.heleket', 'update_heleket_payment', *args, **kwargs)
 
 
 async def link_heleket_payment_to_transaction(*args, **kwargs):
-    heleket_crud = import_module('app.database.crud.heleket')
-    return await heleket_crud.link_heleket_payment_to_transaction(*args, **kwargs)
+    return await _call_crud('app.database.crud.heleket', 'link_heleket_payment_to_transaction', *args, **kwargs)
 
 
 async def create_cloudpayments_payment(*args, **kwargs):
-    cloudpayments_crud = import_module('app.database.crud.cloudpayments')
-    return await cloudpayments_crud.create_cloudpayments_payment(*args, **kwargs)
+    return await _call_crud('app.database.crud.cloudpayments', 'create_cloudpayments_payment', *args, **kwargs)
 
 
 async def get_cloudpayments_payment_by_invoice_id(*args, **kwargs):
-    cloudpayments_crud = import_module('app.database.crud.cloudpayments')
-    return await cloudpayments_crud.get_cloudpayments_payment_by_invoice_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.cloudpayments', 'get_cloudpayments_payment_by_invoice_id', *args, **kwargs)
 
 
 async def get_cloudpayments_payment_by_id(*args, **kwargs):
-    cloudpayments_crud = import_module('app.database.crud.cloudpayments')
-    return await cloudpayments_crud.get_cloudpayments_payment_by_id(*args, **kwargs)
+    return await _call_crud('app.database.crud.cloudpayments', 'get_cloudpayments_payment_by_id', *args, **kwargs)
 
 
 async def update_cloudpayments_payment(*args, **kwargs):
-    cloudpayments_crud = import_module('app.database.crud.cloudpayments')
-    return await cloudpayments_crud.update_cloudpayments_payment(*args, **kwargs)
+    return await _call_crud('app.database.crud.cloudpayments', 'update_cloudpayments_payment', *args, **kwargs)
 
 
 class PaymentService(
