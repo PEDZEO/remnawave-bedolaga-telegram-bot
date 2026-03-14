55a131c8c0dfa4e9faf0a306c5ae8c8910bbe026
PEDZEO <vitali.gavrikov@yandex.ru>
2026-03-14 21:28:27 +0300

    fix: support non-committing notification cleanup in subscription flow

--- app/database/crud/notification.py
+++ app/database/crud/notification.py
@@ -48,9 +48,12 @@ async def record_notification(
     await db.commit()
 
 
-async def clear_notifications(db: AsyncSession, subscription_id: int) -> None:
+async def clear_notifications(db: AsyncSession, subscription_id: int, *, commit: bool = True) -> None:
     await db.execute(delete(SentNotification).where(SentNotification.subscription_id == subscription_id))
-    await db.commit()
+    if commit:
+        await db.commit()
+    else:
+        await db.flush()
 
 
 async def clear_notification_by_type(
